5851ca6b99b4c35a0f40d190c0b5d5892ec606c4
mohsetiawanmodeong <mohsetiawanmodeong@gmail.com>
2022-04-08 11:21:22 +0800

    buat api

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class ApiController extends Controller
+{
+    public function login(Request $request)
+    {
+        $login = Auth::Attempt($request->all());
+        if ($login) {
+            $user = Auth::user();
+            $user->api_token = Str::random(100);
+            $user->save();
+            // $user->makeVisible('api_token');
+
+            return response()->json([
+                'response_code' => 200,
+                'message' => 'Login Berhasil',
+                'conntent' => $user
+            ]);
+        } else {
+            return response()->json([
+                'response_code' => 404,
+                'message' => 'Username atau Password Tidak Ditemukan!'
+            ]);
+        }
+    }
+}
