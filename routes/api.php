d12ce2df6befdd1de957180e0fea2139020bb413
mohsetiawanmodeong <mohsetiawanmodeong@gmail.com>
2022-04-08 11:39:49 +0800

    add url api

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('login', [ApiController::class, 'login']);
+Route::post('https://api.kotamobagu.go.id/login.php', [ApiController::class, 'login']);
